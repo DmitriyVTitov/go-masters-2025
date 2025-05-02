7c5b3c5e973105255b803dca5294d25ff424c8b7
Dmitriy Titov <dtsp@yandex.ru>
2025-05-02 16:26:39 +0300

    Test Examples

--- 06-testing_debug/3-pprof/pprof_main.go
+++ 06-testing_debug/3-pprof/pprof_main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	_ "net/http/pprof" // автоматически регистрирует обработчики в DefaultServeMux
 	"sync"
-	"time"
 )
 
 // Вызов профиля:
@@ -31,7 +30,7 @@ func cpuHandler(w http.ResponseWriter, r *http.Request) {
 	wg.Add(4)
 
 	f := func() {
-		for i := range 1_000_000 {
+		for i := range 10_000_000 {
 			_ = math.Sin(float64(i))
 		}
 
@@ -50,11 +49,9 @@ func memHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
 
 	buf := make([]byte, 1_000_000_000)
-	for i := 0; i < len(buf); i++ {
+	for i := range len(buf) {
 		buf[i] = byte(i % 256)
 	}
-
-	time.Sleep(time.Second * 30)
 
 	for i := range len(buf) {
 		_ = buf[i]
